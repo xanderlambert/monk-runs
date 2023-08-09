a11f6e3efbd0bbe1eae3a7746bb24edecd3bb81a
Alex <xanderlambert@gmail.com>
2023-08-09 17:22:59 -0500

    monk runs setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script> 
+let title = `New Orleans Monk Runs`;
+</script>
+
+<nav>
+	<a href="/revelations">revelations</a>
+</nav>
+
+<div class="index">
+  <h1>{title}</h1>
+  <p>Visit the original r/NewOrleans post <a href="https://www.reddit.com/r/NewOrleans/comments/qgj4pk/old_baker_moment_in_nola/"> here</a>.</p>
+
+</div>
+
+
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/revelations/+page.svelte
@@ -0,0 +1,19 @@
+<script></script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/">somewhere else</a>
+</nav>
+
+<div class='revelations'>
+  <h1>Revelations:</h1>
+</div>
+
+
+<style>
+  .revelations {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
