c946d267aab15bdcbe5a9f0a8171e11ea59fa19f
Alex <xanderlambert@gmail.com>
2023-08-14 14:15:12 -0500

    add religious iconography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,14 @@ let title = `New Orleans Monk Runs`;
 </script>
 
 <div class='titleContainer'>
-  <h1 class='title' >{title}</h1>
 
   <div> 
+    <!-- svelte-ignore a11y-img-redundant-alt -->
+    <img src="src\stainedGlassRunningIcon_final.png" alt="Your Image Description">
   </div>
+  <h1 class='title' >{title}</h1>
+
+  
   
 </div>
 
@@ -20,28 +24,38 @@ let title = `New Orleans Monk Runs`;
     display: block;
     font-family: "Georgia", Times, serif;
     color: #87a8b1;
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   }
 
   .titleContainer {
-    background-color: #87a8b1;
-  background: rgba( 255, 255, 255, 0.15 );
-  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-  backdrop-filter: blur( 18px );
-  -webkit-backdrop-filter: blur( 18px );
-  border: 1px solid rgba( 255, 255, 255, 0.18 );
-  border-radius: 1rem;
-  padding: 1.5rem;
-  z-index: 10;
-  
-  }
+      background: radial-gradient(ellipse at center, rgba(255, 204, 0, 0.5) 0%, rgba(255, 102, 0, 0.5) 100%);
+      border: 0.25em solid #1a1919fd; /* Solid black border */
+      border-radius: 1rem;
+      padding: 1.5rem;
+      margin: 1rem;
+      z-index: 10;
+      backdrop-filter: blur(18px);
+      -webkit-backdrop-filter: blur(18px);
+    }
 
   .nav {
     margin: 2em; 
     color: #87a8b1;
   }
-
+  
   .link {
     text-decoration: none; 
     color: inherit;
+    background: radial-gradient(ellipse at center, rgba(255, 204, 0, 0.5) 0%, rgba(255, 102, 0, 0.5) 100%);
+      border: 0.25em solid #1a1919fd; /* Solid black border */
+      border-radius: 1rem;
+      padding: 1.5rem;
+      margin: 1rem;
+      z-index: 10;
+      backdrop-filter: blur(18px);
+      -webkit-backdrop-filter: blur(18px);
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
   }
+
+
 </style>
